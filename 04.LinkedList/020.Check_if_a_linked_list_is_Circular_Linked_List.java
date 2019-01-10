7e259ea7671e5a7d579349c9e817b8e0ef489df2
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-01-11 02:50:28 +0530

    Update 020.Check_if_a_linked_list_is_Circular_Linked_List.java

--- 04.LinkedList/020.Check_if_a_linked_list_is_Circular_Linked_List.java
+++ 04.LinkedList/020.Check_if_a_linked_list_is_Circular_Linked_List.java
@@ -63,3 +63,56 @@ class LinkedList {
        
     } 
 }
+/*
+class LinkedList 
+{ 
+	static Node head; // head of the list 
+
+	
+	static class Node 
+	{ 
+		int data; 
+		Node next; 
+		Node(int d) 
+		{ 
+			data = d; 
+			next = null; 
+		} 
+	} 
+
+	
+
+        public boolean isCircular(Node node ){
+         if(node==null){
+           return false;
+          }
+           
+          Node temp=node.next;
+          while(temp!=null && temp!=node){
+          temp=temp.next;
+          }
+          return (temp==head);
+        }
+
+        
+	public void push(int new_data) 
+	{ 
+		Node new_node = new Node(new_data); 
+		new_node.next = head; 
+		head = new_node; 
+	} 
+
+	public static void main(String [] args) 
+	{ 
+		LinkedList llist = new LinkedList(); 
+		llist.push(20); 
+		llist.push(4); 
+		llist.push(15); 
+		llist.push(35); 
+                llist.head.next.next.next.next=llist.head;
+                System.out.println(llist.isCircular(head));
+		
+	} 
+}
+
+*/
