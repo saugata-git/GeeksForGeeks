f18fcfa5edadf6ae6b06caea5131180f000b92cf
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-01-17 03:11:14 +0530

    Create 003.Convert_Ternary_Expression_to_Binary_Tree.java

---
+++ 05.Tree/003.Convert_Ternary_Expression_to_Binary_Tree.java
@@ -0,0 +1,59 @@
+// Function to convert Ternary Expression to a Binary 
+//O(n) [ here n is length of String ]
+class Node  
+{ 
+    char data; 
+    Node left, right; 
+   
+    public Node(char item)  
+    { 
+        data = item; 
+        left = null; 
+        right = null; 
+    } 
+} 
+   
+ 
+class BinaryTree  
+{ 
+    /*
+      Input :  string expression =   a?b:c 
+      Output : a
+              /  \
+             b    c
+    */
+    
+    Node convertExpression(char[] expression, int i) 
+    {  
+        if (i >= expression.length) 
+            return null; 
+        Node root = new Node(expression[i]); 
+        ++i; 
+        if (i < expression.length && expression[i]=='?') 
+            root.left = convertExpression(expression, i+1); 
+        else if (i < expression.length) 
+            root.right = convertExpression(expression, i+1); 
+        return root; 
+    } 
+      
+  
+    public void printTree( Node root) 
+    { 
+        if (root == null) 
+            return; 
+                  
+        System.out.print(root.data +" "); 
+        printTree(root.left); 
+        printTree(root.right); 
+    } 
+      
+
+    public static void main(String args[])  
+    { 
+        String exp = "a?b?c:d:e"; 
+        BinaryTree tree = new BinaryTree(); 
+        char[] expression=exp.toCharArray();  
+        Node root = tree.convertExpression(expression, 0); 
+        tree.printTree(root) ; 
+    } 
+} 
