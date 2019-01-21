4f8557400ba62796ca41cf77b18dbfc6fa44a933
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-01-22 04:43:01 +0530

    Update and rename 021.Root to Leaf Paths.java to 023.Root to Leaf Paths.java

--- 05.Tree/023.Root to Leaf Paths.java
+++ 05.Tree/023.Root to Leaf Paths.java
@@ -1,5 +1,5 @@
 // Java program to print all the node to leaf path 
-
+//Time Complexity: O(n2) where n is number of nodes.
 /* A binary tree node has data, pointer to left child 
 and a pointer to right child */
 class Node 
