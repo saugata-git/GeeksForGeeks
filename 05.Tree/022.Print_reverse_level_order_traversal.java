fc53992912c5f702c28d662fcf7dd96f7865b30d
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-01-22 04:20:49 +0530

    Create 022.Print_reverse_level_order_traversal.java

---
+++ 05.Tree/022.Print_reverse_level_order_traversal.java
@@ -0,0 +1,70 @@
+// A recursive java program to print reverse level order traversal 
+// using stack and queue 
+//Time Complexity: O(n) where n is number of nodes in the binary tree.
+
+
+import java.util.*; 
+class Node 
+{ 
+	int data; 
+	Node left, right; 
+
+	Node(int item) 
+	{ 
+		data = item; 
+		left = right; 
+	} 
+} 
+
+class BinaryTree 
+{ 
+	Node root; 
+
+	void reverseLevelOrder(Node node) 
+	{ 
+		Stack<Node> S = new Stack(); 
+		Queue<Node> Q = new LinkedList(); 
+		Q.add(node); 
+
+		
+		while (Q.isEmpty() == false) 
+		{ 
+			
+			node = Q.peek(); 
+			Q.remove(); 
+			S.push(node); 
+
+			if (node.right != null)  
+				Q.add(node.right); 
+			if (node.left != null) 
+				Q.add(node.left); 
+		} 
+
+		while (S.empty() == false) 
+		{ 
+			node = S.peek(); 
+			System.out.print(node.data + " "); 
+			S.pop(); 
+		} 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		BinaryTree tree = new BinaryTree(); 
+
+		
+		tree.root = new Node(1); 
+		tree.root.left = new Node(2); 
+		tree.root.right = new Node(3); 
+		tree.root.left.left = new Node(4); 
+		tree.root.left.right = new Node(5); 
+		tree.root.right.left = new Node(6); 
+		tree.root.right.right = new Node(7); 
+
+		System.out.println("Level Order traversal of binary tree is :"); 
+		tree.reverseLevelOrder(tree.root); 
+
+	} 
+} 
+
+
