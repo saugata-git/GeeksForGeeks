7e709c84ddff5544b216f9eed2c70ab631198526
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-01-30 22:02:07 +0530

    Update and rename a to 000.Sum_of_dependencies_in_graph.java

---
+++ 10.Graph/000.Sum_of_dependencies_in_graph.java
@@ -0,0 +1,41 @@
+// Java program to find the sum of dependencies 
+import java.io.*;
+import java.util.Vector; 
+
+public class Graph
+{ 
+	
+	static void addEdge(Vector <Integer> adj[], int u,int v) 
+	{ 
+		adj[u].addElement((v)); 
+	} 
+	
+	static int findSum(Vector<Integer> adj[], int V) 
+	{ 
+		int sum = 0; 
+		for (int u = 0; u < V; u++) 
+			sum += adj[u].size(); 
+	
+		return sum; 
+	} 
+	
+
+	public static void main(String[] args) 
+	{ 
+		int V = 4; 
+		Vector<Integer> adj[] = new Vector[V]; 
+		
+		for (int i = 0; i < adj.length; i++) { 
+			adj[i] = new Vector<>(); 
+		} 
+		
+		addEdge(adj, 0, 2); 
+		addEdge(adj, 0, 3); 
+		addEdge(adj, 1, 3); 
+		addEdge(adj, 2, 3); 
+	
+		System.out.println("Sum of dependencies is " + findSum(adj, V)); 
+	} 
+} 
+
+
