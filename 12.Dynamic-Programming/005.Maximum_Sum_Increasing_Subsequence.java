cbfec1f0ac9302c692e8a2dc947dee97d2cba8bc
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-02-09 15:34:18 +0530

    Create 005.Maximum_Sum_Increasing_Subsequence.java

---
+++ 12.Dynamic-Programming/005.Maximum_Sum_Increasing_Subsequence.java
@@ -0,0 +1,29 @@
+//Maximum Sum Increasing Subsequence
+Time Complexity: O(n^2)
+class dp{
+ 
+    static int maxSumIS(int arr[],int n){
+     int i,j,max=0;
+     int msis[]=new int[n];
+
+     for(i=0;i<n;i++)
+       msis[i]=arr[i];
+
+     for(i=1;i<n;i++)
+        for(j=0;j<i;j++)
+           if(arr[i]>arr[j] && msis[i]<msis[j]+arr[i])
+               msis[i]=msis[j]+arr[i];
+
+     for(i=0;i<n;i++)
+        if(max<msis[i])
+          max=msis[i];
+
+     return max;
+    }
+  
+    public static void main(String args[]){ 
+       int arr[] =new int[]{1, 101, 2, 3, 100, 4, 5};
+       int n=arr.length;
+       System.out.println("Sum of maximum sum "+ "increasing subsequence is "+ maxSumIS(arr, n));
+    } 
+} 
