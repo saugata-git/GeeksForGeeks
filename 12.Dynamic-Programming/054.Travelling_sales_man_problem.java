6eb76c8fa6dbe64bdfe450c9512f89a7a92f0809
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-03-10 11:09:31 +0530

    Update 054.Travelling_sales_man_problem.java

--- 12.Dynamic-Programming/054.Travelling_sales_man_problem.java
+++ 12.Dynamic-Programming/054.Travelling_sales_man_problem.java
@@ -1,50 +1,43 @@
 import java.util.*;
-import java.lang.Math;
-public class tsp{
-    
+public class tsp1{
 
-    public static int  tsp(int dist[][],int mask,int pos,int n){
+   public static int tsp(int dist[][],int mask,int pos){ 
 
-      int VISITED_ALL = (1<<n) -1;
-      int  var_2_pow_n=(int)Math.pow(2,n);
-      int dp[][]=new int[var_2_pow_n][4];
+      int n=dist.length;
+      int visited_all=(1<<n)-1;
+      int var_2_pow_n=(int)Math.pow(2,n);
+      int dp[][]=new int[var_2_pow_n][n];
 
-        for(int i=0;i<(1<<n);i++){
-          for(int j=0;j<n;j++){
-             dp[i][j] = -1;
-          }
-        }
-	
-	   if(mask==VISITED_ALL){
-		     return dist[pos][0];
-	   }
-	   if(dp[mask][pos]!=-1){
-	       return dp[mask][pos];
-	   }	
-	   int ans = Integer.MAX_VALUE;
-     
-  	for(int city=0;city<n;city++){
-	         	if((mask&(1<<city))==0){
-			             int newAns = dist[pos][city] + tsp(dist ,mask|(1<<city), city,n);
-			             ans = Math.min(ans, newAns);
-		        }
-   	}
-	
-	   return dp[mask][pos] = ans;
-   } 
-      
+      for(int i=0;i<(1<<n);i++){
+         for(int j=0;j<n;j++){
+          dp[i][j]=-1;
+         }
+      }
+       
+      if(mask==visited_all){
+       return dist[pos][0];
+      }
+      if(dp[mask][pos]!=-1){
+       return dp[mask][pos];
+      }
+      int ans=Integer.MAX_VALUE;
 
+      for(int city=0;city<n;city++){
+            if((mask&(1<<city))==0){                // checks for particular edge bit mask b->0010
+              int newAns=dist[pos][city]+tsp(dist,mask|(1<<city),city);// checks for a->0001 b->0010 ,path from a-to-b ->0011
+               ans=Math.min(ans,newAns);
+            }
+      }
 
-  
-  public static void main(String args[]){
-    int n=4;
+     return dp[mask][pos]=ans;
+
+   }
+   public static void main(String args[]){
+
     int dist[][] = { {0,20,42,25},
                      {20,0,30,34},
                      {42,30,0,10},
                      {25,34,10,0}
                    };
-      
-   System.out.println("Travelling Saleman Distance is "+ tsp(dist,1,0,n));
-   
-  }
-}
+    
+    System.out.println("Travelling Saleman Distance is "+ tsp(dist,1,0));  //1->0001,0->starting edge
