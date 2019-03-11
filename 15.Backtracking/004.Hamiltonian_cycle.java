7691289cbfab16e355eaae2e8879a2328e8825a6
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-03-11 22:50:03 +0530

    Create 004.Hamiltonian_cycle.java

---
+++ 15.Backtracking/004.Hamiltonian_cycle.java
@@ -0,0 +1,81 @@
+//Worst case complexity of using DFS and backtracking is O(N!).
+
+class HC{
+
+   final int V=5;
+   int path[];
+
+   boolean isSafe(int v,int graph[][],int path[],int pos){
+      
+         if(graph[path[pos-1]][v]==0)
+            return false;
+
+         for(int i=0;i<pos;i++)
+            if(path[i]==v) 
+               return false;
+
+         return true;
+   }
+
+
+   boolean hamCycleUtil(int graph[][],int path[],int pos){
+
+      if(pos==V){
+            if(graph[path[pos-1]][path[0]]==1)
+                return true;
+            else
+                return false;
+      }
+
+      for(int v=1;v<V;v++){       
+          if(isSafe(v,graph,path,pos)){
+                path[pos]=v;
+                if(hamCycleUtil(graph,path,pos+1)==true)
+                    return true;
+                path[pos]=-1;
+          }
+      }
+  
+      return false;
+   }
+
+
+
+   int hamCycle(int graph[][]){
+
+     path=new int[V];
+     for(int i=0;i<V;i++)
+          path[i]=-1;
+     path[0]=0;
+     if(hamCycleUtil(graph,path,1)==false){
+         System.out.println("\n Solution doesn't exist");
+         return 0;
+     }
+     printSolution(path);
+     return 1;
+   }
+
+   void printSolution(int path[]){
+   
+          System.out.println("Solution Exists: Following" + " is one Hamiltonian Cycle"); 
+          for(int i=0;i<V;i++)
+              System.out.print(" "+path[i]+" ");
+          System.out.print(" "+path[0]+" ");
+   }
+
+
+
+   public static void main(String args[]){
+     
+        HC hc =new HC();
+        int graph[][]={{0, 1, 0, 1, 0}, 
+		       {1, 0, 1, 1, 1}, 
+		       {0, 1, 0, 0, 1}, 
+		       {1, 1, 0, 0, 1}, 
+		       {0, 1, 1, 1, 0}, 
+		      }; 
+        hc.hamCycle(graph);
+
+   }
+
+}
