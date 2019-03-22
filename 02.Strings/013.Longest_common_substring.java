0cb311b93cf92ad5642e9a20ab58dd34569b1876
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-03-22 09:10:18 +0530

    Create 013.Longest_common_substring.java

---
+++ 02.Strings/013.Longest_common_substring.java
@@ -0,0 +1,31 @@
+public class LongestCommonSubstring{
+
+  static int LCSubStr(char X[],char Y[],int m,int n){
+
+     int table[][]=new int[m+1][n+1];
+     int result=0;
+
+     for(int i=0;i<=m;i++){
+         for(int j=0;j<=n;j++){
+            if(i==0||j==0)
+                table[i][j]=0;
+            else if(X[i-1]==Y[j-1]){
+               table[i][j]=table[i-1][j-1]+1;
+               result=Integer.max(result,table[i][j]);
+            }
+            else
+              table[i][j]=0;
+         }
+     }
+    return result;
+  }
+
+  public static void main(String args[]){
+   String X="abcdefrg";
+   String Y="cdeg";
+   int m=X.length();
+   int n=Y.length();
+   System.out.println("Length of Longest Common Substring is  " +LCSubStr(X.toCharArray(),Y.toCharArray(),m,n));
+
+  }
+}
