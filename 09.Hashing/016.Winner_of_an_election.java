0fccc921a75545155fe6c92a6a008e7805fcd323
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-04-01 11:19:58 +0530

    Create 016.Winner_of_an_election.java

---
+++ 09.Hashing/016.Winner_of_an_election.java
@@ -0,0 +1,46 @@
+/*
+Winner of an election
+*/
+import java.util.*;
+public class Election{
+
+
+  public static void findWinner(String votes[]){
+
+    Map<String,Integer> map= new HashMap<String,Integer>();
+    for(String str :votes){
+      if(map.containsKey(str)){
+        map.put(str,map.get(str)+1);
+      }
+      else
+       map.put(str,1);
+    }
+    int maxValueInMap=0;
+    String winner="";
+    for(Map.Entry<String,Integer> entry:map.entrySet()){
+         
+        String key =entry.getKey();
+        Integer val=entry.getValue(); 
+        if(val>maxValueInMap){
+            maxValueInMap=val;
+            winner =key;
+        }
+        else if(val==maxValueInMap && winner.compareTo(key)>0){
+          winner=key;
+        }
+
+    }
+     System.out.println(winner);
+  }
+
+  public static void main(String args[]){
+
+     String [] votes={   "john", "johnny", "jackie", 
+                         "johnny", "john", "jackie", 
+                         "jamie", "jamie", "john", 
+                         "johnny", "jamie", "johnny", 
+                         "john"  };
+      findWinner(votes);
+  }
+
+}
