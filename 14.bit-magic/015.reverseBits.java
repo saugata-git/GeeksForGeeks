1bf2913ed0af0bf853a66938fb110a89409deaf5
saugata debnath <31741625+saugata001@users.noreply.github.com>
2019-05-16 10:26:41 +0530

    Update 015.reverseBits.java

--- 14.bit-magic/015.reverseBits.java
+++ 14.bit-magic/015.reverseBits.java
@@ -1,6 +1,7 @@
 class reverseBits{
 
- public static int reverseBits(int n) {
+    /*O(n) solution
+    public static int reverseBits(int n) {
 	for (int i = 0; i < 16; i++) {
 		n = swapBits(n, i, 32 - i - 1);
 	}
@@ -17,10 +18,37 @@ class reverseBits{
 	}
 	return n; 
      }
+   */
+   //O(log(n)) solution
+    public static int sizeof(int n){
+        
+       int size=0;
+       while(n!=0){
+          n=n<<1;
+         size++;
+       }
+       return size;
+    }
+   
+   public static int reverseBits(int n) {
 
-
+       int count = (sizeof(n)*8)-1;
+       int reverse_num=n;
+       
+       n>>=1;
+       while(n!=0){
+          reverse_num<<=1;
+          reverse_num |= n & 1;
+          n>>=1;
+          count--;
+       }
+      reverse_num<<=count;
+     
+     return reverse_num;
+   }
+ 
     public static void main(String args[]){
-        int x=8;
+        int x=1;
         System.out.println(reverseBits(x)); 
     }
 }
